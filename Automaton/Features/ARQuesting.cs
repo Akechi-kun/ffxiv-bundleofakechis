1c1602d93c95bf1c0d362f74c5724787ebe9af53
Jackson <9527380+Jaksuhn@users.noreply.github.com>
2025-04-28 02:53:13 +0100

    Update ARQuesting.cs

--- Automaton/Features/ARQuesting.cs
+++ Automaton/Features/ARQuesting.cs
@@ -6,7 +6,7 @@ namespace Automaton.Features;
 
 public class ARQuestingConfiguration
 {
-    [BoolConfig] public bool EnableReturnHome = true;
+    [BoolConfig] public bool ReturnHome = true;
 }
 
 [Tweak]
@@ -29,5 +29,5 @@ internal class ARQuesting : ARTweak<ARQuestingConfiguration>
             Log("Skipping post process for character: no seasonal quests available.");
     }
 
-    public override void OnCharacterReadyToPostProcess() => Service.Automation.Start(new DoQuests(_quests, Config.EnableReturnHome), AutoRetainer.FinishCharacterPostProcess);
+    public override void OnCharacterReadyToPostProcess() => Service.Automation.Start(new DoQuests(_quests, Config.ReturnHome), AutoRetainer.FinishCharacterPostProcess);
 }
