e9323743271b76b03bd6b2e8c2c708a9c30a8fbd
Jackson <9527380+Jaksuhn@users.noreply.github.com>
2025-06-08 06:53:28 +0100

    only enable when you don't have ED

--- Automaton/Features/MaxGCRank.cs
+++ Automaton/Features/MaxGCRank.cs
@@ -7,8 +7,20 @@ namespace Automaton.Features;
 internal class MaxGCRank : Tweak
 {
     public override string Name => "Enforce Expert Delivery";
-    public override string Description => "Automatically maxes your GC rank to force the expert delivery window to show. Does not bypass anything else rank-restricted.";
+    public override string Description => "Automatically maxes your GC rank to force the expert delivery window to show. Does not bypass anything else rank-restricted. Only in effect if you do not have expert delivery unlocked.";
 
-    internal unsafe EzHook<PlayerState.Delegates.GetGrandCompanyRank>? Hook = new((nint)PlayerState.MemberFunctionPointers.GetGrandCompanyRank, Detour, false);
-    internal static unsafe byte Detour(PlayerState* thisPtr) => 17;
+    public override unsafe void Enable()
+    {
+        Hook = new((nint)PlayerState.MemberFunctionPointers.GetGrandCompanyRank, Detour, false);
+        Hook.Enable();
+    }
+
+    public override void Disable() => Hook?.Disable();
+
+    internal unsafe EzHook<PlayerState.Delegates.GetGrandCompanyRank>? Hook;
+    internal unsafe byte Detour(PlayerState* thisPtr)
+    {
+        var ret = Hook!.Original(thisPtr);
+        return ret < 6 ? (byte)17 : ret;
+    }
 }
