33794a73199cd13c0e8bf3901bd587402c842ad6
Jackson <9527380+Jaksuhn@users.noreply.github.com>
2025-09-07 16:37:49 +0100

    Update AutoFollow.cs

--- Automaton/Features/AutoFollow.cs
+++ Automaton/Features/AutoFollow.cs
@@ -97,7 +97,9 @@ public unsafe class AutoFollow : Tweak<AutoFollowConfiguration>
         if (!Player.Available || TaskManager.IsBusy) return;
         if (_masterId == null && Config.AutoFollowName.IsNullOrEmpty() && string.IsNullOrEmpty(_masterName)) return; // always try to follow if temp or permanent name is set
 
-        var master = Svc.Objects.FirstOrDefault(x => x.EntityId == _masterId || !Config.AutoFollowName.IsNullOrEmpty() && x.Name.TextValue.Equals(Config.AutoFollowName, StringComparison.InvariantCultureIgnoreCase));
+        var master = Svc.Objects.FirstOrDefault(x => x.EntityId == _masterId
+            || (!Config.AutoFollowName.IsNullOrEmpty() && x.Name.TextValue.Equals(Config.AutoFollowName, StringComparison.InvariantCultureIgnoreCase))
+            || (!string.IsNullOrEmpty(_masterName) && x.Name.TextValue.Equals(_masterName, StringComparison.InvariantCultureIgnoreCase));
 
         if (master == null) { movement.Enabled = false; return; }
         if (Config.DisableIfFurtherThan > 0 && Player.DistanceTo(master) >= Config.DisableIfFurtherThan) { movement.Enabled = false; return; }
