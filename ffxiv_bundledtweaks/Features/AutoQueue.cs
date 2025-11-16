80d0545c6c5d42af0d2573faff904c77802da35b
Jackson <9527380+Jaksuhn@users.noreply.github.com>
2025-11-16 12:21:51 +0000

    ancient code

--- ffxiv_bundledtweaks/Features/AutoQueue.cs
+++ ffxiv_bundledtweaks/Features/AutoQueue.cs
@@ -1,5 +1,7 @@
 using ECommons.ExcelServices;
+using FFXIVClientStructs.FFXIV.Client.Game.UI;
 using FFXIVClientStructs.FFXIV.Client.System.Framework;
+using FFXIVClientStructs.FFXIV.Client.UI.Agent;
 
 namespace ComplexTweaks.Features;
 
@@ -9,19 +11,8 @@ internal class AutoQueue : Tweak
     public override string Name => "Auto Queue";
     public override string Description => "Auto queue into a pre-checked duty.\nTriggers on zone change.\n If everyone in the party is nearby each other in the overworld (targetable range), it will wait for them to be fully loaded into the overworld first.";
 
-    public override void Enable()
-    {
-        Svc.ClientState.TerritoryChanged += OnTerritoryChanged;
-        Svc.AddonLifecycle.RegisterListener(AddonEvent.PostSetup, "ContentsFinder", OnSetupContentsFinder);
-    }
-
-    public override void Disable()
-    {
-        Svc.ClientState.TerritoryChanged -= OnTerritoryChanged;
-        Svc.AddonLifecycle.UnregisterListener(OnSetupContentsFinder);
-    }
-
-    private unsafe void OnSetupContentsFinder(AddonEvent type, AddonArgs args) => Callback.Fire(args.ToPtr(), true, 12, 0);
+    public override void Enable() => Svc.ClientState.TerritoryChanged += OnTerritoryChanged;
+    public override void Disable() => Svc.ClientState.TerritoryChanged -= OnTerritoryChanged;
 
     private unsafe void OnTerritoryChanged(ushort obj)
     {
@@ -29,6 +20,24 @@ internal class AutoQueue : Tweak
         TaskManager.Enqueue(() => !IsOccupied());
         TaskManager.Enqueue(() => Svc.Party.All(p => p.Territory.Value.TerritoryIntendedUse.Value.RowId is (byte)TerritoryIntendedUseEnum.City_Area or (byte)TerritoryIntendedUseEnum.Open_World));
         TaskManager.Enqueue(() => !Svc.Party.All(p => p.Territory.Value.RowId == Player.Territory) || Svc.Party.All(p => p.GameObject?.IsTargetable ?? false));
-        TaskManager.Enqueue(() => Framework.Instance()->GetUIModule()->ExecuteMainCommand(33));
+        TaskManager.Enqueue(QueueSelectedDuty);
+    }
+
+    private unsafe bool QueueSelectedDuty()
+    {
+        var ids = AgentContentsFinder.Instance()->SelectedContent.Select(x => x.Id).ToList();
+        var array = stackalloc uint[ids.Count];
+        for (var i = 0; i < ids.Count; i++)
+            array[i] = ids[i];
+        if (AgentContentsFinder.Instance()->SelectedContent.Any(x => x.ContentType is ContentsId.ContentsType.Roulette))
+        {
+            ContentsFinder.Instance()->QueueInfo.QueueRoulette((byte)AgentContentsFinder.Instance()->SelectedContent.First().Id);
+            return true;
+        }
+        else
+        {
+            ContentsFinder.Instance()->QueueInfo.QueueDuties(array, ids.Count);
+            return true;
+        }
     }
 }
