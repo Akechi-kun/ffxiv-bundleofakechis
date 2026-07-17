d1c07516d5e3cb003c6bb86ab5b5eec6cc5ff26d
Jackson <9527380+Jaksuhn@users.noreply.github.com>
2026-07-17 12:59:09 +0100

    Update ReQueueCC.cs

--- ffxiv_bundledtweaks/Tweaks/ReQueueCC.cs
+++ ffxiv_bundledtweaks/Tweaks/ReQueueCC.cs
@@ -13,8 +13,16 @@ public class ReQueueCC : Tweak {
     public override void Disable() => Svc.Chat.LogMessage -= CheckForMessage;
 
     private unsafe void CheckForMessage(ILogMessage message) {
-        if (message.LogMessageId is 7392)
-            if (AgentContentsFinder.Instance()->SelectedContent.FirstOrNull(x => x.ContentType is ContentsType.Roulette) is { Id: (40 or 41) and var id })
+        if (message.LogMessageId is 7392) {
+            Log($"Requeueing for CC due to map change.");
+            if (AgentContentsFinder.Instance()->SelectedContent.FirstOrNull(x => x.ContentType is ContentsType.Roulette) is { Id: (40 or 41) and var id }) {
+                Log($"Requeueing for CC with id {id}.");
                 ContentsFinder.Instance()->QueueInfo.QueueRoulette((byte)id);
+            }
+            else {
+                Log($"Requeueing for casual CC. Unable to detect selected content.");
+                ContentsFinder.Instance()->QueueInfo.QueueRoulette(40);
+            }
+        }
     }
 }
