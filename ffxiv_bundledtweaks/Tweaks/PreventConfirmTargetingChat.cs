ded04b4f720cbc9389cd34b9b737bb76e79698d8
Jackson <9527380+Jaksuhn@users.noreply.github.com>
2026-07-23 17:27:54 +0100

    tweak of the year

---
+++ ffxiv_bundledtweaks/Tweaks/PreventConfirmTargetingChat.cs
@@ -0,0 +1,28 @@
+﻿using FFXIVClientStructs.FFXIV.Client.UI;
+using FFXIVClientStructs.FFXIV.Component.GUI;
+
+namespace Automaton.Tweaks;
+
+[Tweak]
+public partial class PreventConfirmTargetingChat : Tweak {
+    public override string Name => "Prevent Confirm Targeting Chat";
+    public override string Description => "If the confirm keybind would target the chat window, prevent it.";
+
+    [AddressHook<AtkModule>(nameof(AtkModule.MemberFunctionPointers.HandleInput))]
+    public unsafe byte HandleInput(AtkModule* atkModule, UIInputData* inputData, bool isPadMouseModeEnabled) {
+        try {
+            if (inputData->IsInputIdPressed(FFXIVClientStructs.FFXIV.Client.System.Input.InputId.OK)) {
+                if (atkModule->AtkUnitManager->FocusedAddon is not null and var f && f->NameString == "ChatLog") {
+                    atkModule->AtkUnitManager->FocusedAddon = null;
+                    atkModule->AtkStage->ClearFocus();
+                }
+            }
+
+            return HandleInputHook.Original(atkModule, inputData, isPadMouseModeEnabled);
+        }
+        catch (Exception ex) {
+            Error(ex, $"Error setting focused to chat to null");
+            return HandleInputHook.Original(atkModule, inputData, isPadMouseModeEnabled);
+        }
+    }
+}
