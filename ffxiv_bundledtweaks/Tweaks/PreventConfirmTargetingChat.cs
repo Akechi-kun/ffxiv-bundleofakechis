468785534340414af56bc47a0abcf00ad90ccbb1
Jackson <9527380+Jaksuhn@users.noreply.github.com>
2026-09-18 08:58:14 +0100

    Update PreventConfirmTargetingChat.cs

--- ffxiv_bundledtweaks/Tweaks/PreventConfirmTargetingChat.cs
+++ ffxiv_bundledtweaks/Tweaks/PreventConfirmTargetingChat.cs
@@ -13,15 +13,20 @@ public partial class PreventConfirmTargetingChat : Tweak {
         var ret = HandleInputHook.Original(atkModule, inputData, isPadMouseModeEnabled);
         if (atkModule is null || inputData is null)
             return ret;
+        if (atkModule->AtkUnitManager is null || atkModule->AtkUnitManager->FocusedAddon is null)
+            return ret;
 
         try {
-            if (inputData->IsInputIdPressed(InputId.OK) && atkModule->AtkUnitManager is not null and var unitManager && unitManager->FocusedAddon is not null and var focusedAddon && focusedAddon->NameString == "ChatLog") {
-                unitManager->FocusedAddon = null;
+            if (inputData->IsInputIdPressed(InputId.OK) && atkModule->AtkUnitManager->FocusedAddon->NameString == "ChatLog") {
+                if (atkModule->IsTextInputActive()) // don't lose focus if you're actually typing in the chat
+                    return ret;
+
+                atkModule->AtkUnitManager->FocusedAddon = null;
                 if (atkModule->AtkStage is not null)
                     atkModule->AtkStage->ClearFocus();
             }
 
-            return ret;
+            return HandleInputHook.Original(atkModule, inputData, isPadMouseModeEnabled);
         }
         catch (Exception ex) {
             Error(ex, "Error clearing chat focus");
